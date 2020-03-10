b20b2e3926adffb1ebcbd36721cecfd2e9da2a29
Emma Wedekind <emma.wedekind@logmein.com>
2020-03-10 11:42:26 -0500

    Animating components

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useSpring, animated, config } from "react-spring";
 import { PrimaryButton, SecondaryButton } from "./Buttons";
 import { EmailInput, PasswordInput } from "./TextFields";
 import { Illustrations, CloseIcon } from "../assets";
@@ -48,43 +49,57 @@ const CloseModalButton = styled.button`
   padding: 0;
 `;
 
-export const SignUpModal = () => {
+const getAnimation = showModal => {
+  return {
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? `translateY(0)` : `translateY(-200%)`,
+    config: config.slow
+  };
+};
+
+export const SignUpModal = ({ showModal, setShowModal }) => {
+  const animation = useSpring(getAnimation(showModal));
   return (
-    <ModalWrapper>
-      <img src={Illustrations.SignUp} alt="Sign up for an account!" />
-      <ModalHeader>Sign Up</ModalHeader>
-      <SignUpText>
-        Sign up today to get access to all of our content and features!
-      </SignUpText>
-      <PrimaryButton onClick={() => console.log("You signed up!")}>
-        Sign Up
-      </PrimaryButton>
-      <CloseModalButton
-        aria-label="Close modal"
-        onClick={() => console.log("You closed the modal!")}
-      >
-        <CloseIcon />
-      </CloseModalButton>
-    </ModalWrapper>
+    <animated.div style={animation}>
+      <ModalWrapper>
+        <img src={Illustrations.SignUp} alt="Sign up for an account!" />
+        <ModalHeader>Sign Up</ModalHeader>
+        <SignUpText>
+          Sign up today to get access to all of our content and features!
+        </SignUpText>
+        <PrimaryButton onClick={() => console.log("You signed up!")}>
+          Sign Up
+        </PrimaryButton>
+        <CloseModalButton
+          aria-label="Close modal"
+          onClick={() => setShowModal(false)}
+        >
+          <CloseIcon />
+        </CloseModalButton>
+      </ModalWrapper>
+    </animated.div>
   );
 };
 
-export const SignInModal = () => {
+export const SignInModal = ({ showModal, setShowModal }) => {
+  const animation = useSpring(getAnimation(showModal));
   return (
-    <ColumnModalWrapper>
-      <div>
-        <ModalHeader>Sign In</ModalHeader>
-        <EmailInput label="Email" placeholder="emmabostian@gmail.com" />
-        <PasswordInput label="Password" />
-        <SecondaryButton style={{ margin: "16px 16px 0 0" }}>
-          Sign Up
-        </SecondaryButton>
-        <PrimaryButton>Sign In</PrimaryButton>
-      </div>
-      <img src={Illustrations.SignIn} alt="Sign in to your account" />
-      <CloseModalButton onClick={() => console.log("You closed the modal!")}>
-        <CloseIcon />
-      </CloseModalButton>
-    </ColumnModalWrapper>
+    <animated.div style={animation}>
+      <ColumnModalWrapper>
+        <div>
+          <ModalHeader>Sign In</ModalHeader>
+          <EmailInput label="Email" placeholder="emmabostian@gmail.com" />
+          <PasswordInput label="Password" />
+          <SecondaryButton style={{ margin: "16px 16px 0 0" }}>
+            Sign Up
+          </SecondaryButton>
+          <PrimaryButton>Sign In</PrimaryButton>
+        </div>
+        <img src={Illustrations.SignIn} alt="Sign in to your account" />
+        <CloseModalButton onClick={() => console.log("You closed the modal!")}>
+          <CloseIcon />
+        </CloseModalButton>
+      </ColumnModalWrapper>
+    </animated.div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, darkTheme, defaultTheme } from "./utils";
-import { SignInModal } from "./components";
+import { SignUpModal } from "./components";
 
 const App = () => {
   const [useDarkTheme, setUseDarkTheme] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   return (
     <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
       <button
@@ -20,6 +21,12 @@ const App = () => {
       >
         Default theme
       </button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setShowModal(true)}
+      >
+        Show modal
+      </button>
       <div
         style={{
           background: useDarkTheme
@@ -32,7 +39,7 @@ const App = () => {
           justifyContent: "space-around"
         }}
       >
-        <SignInModal />
+        <SignUpModal showModal={showModal} setShowModal={setShowModal} />
       </div>
       <GlobalStyle />
     </ThemeProvider>
